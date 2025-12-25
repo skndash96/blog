e2a34f1a79285851964f6a1b332375f6da3ed0f3
Dash Skndash <dashskndash@gmail.com>
2025-12-25 09:54:01 +0530

    my name

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site meta description for SEO
+export const SITE_TITLE = 'Dash Skndash S';
+export const SITE_TAGLINE = 'Building web systems that hold up in production.'; // site tagline
+export const SITE_DESCRIPTION = 'Portfolio of a software engineer focused on backend systems, web performance, and real-world production reliability.'; // site meta description for SEO
